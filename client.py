d33e964549f14a1c0721698fad20ce993e3ec931
Anjali Upadhyay <ianjali1598@gmail.com>
2021-06-22 23:23:13 +0530

    files added

---
+++ client.py
@@ -0,0 +1,19 @@
+import requests
+
+import json
+operator=input('''Type one of the opertaors from these options:
+                 + : For addition
+                 - : For substraction
+                 * : For multiplication
+                 / : For Divison
+
+                 Type here: ''')
+
+num1 =int(input('Enter First Number:'))
+num2=int(input('Enter Second Number:'))
+data={'operator':'','num1':'','num2':''}
+data['operator']=operator
+data['num1']=num1
+data['num2']=num2
+r1=requests.post(url='http://localhost:8080/',data=json.dumps(data))
+# r2=requests.get(url='http://localhost:8080/')
