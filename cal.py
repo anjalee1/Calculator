f5ddae513f25357d43e333a185b598d20311ac79
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-06-23 19:40:50 +0530

    Update cal.py

--- cal.py
+++ cal.py
@@ -3,7 +3,7 @@ import cgi
 
 result =['']
 class CalHandler(BaseHTTPRequestHandler):
-    result = ''
+    
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
